5496734630d14da2437ed2d105be6f9a879f617c
mdothan <32659022+Minbiao-Han@users.noreply.github.com>
2019-04-13 16:42:24 -0400

    Create wganscript.sh

---
+++ wganscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python main.py --dataset mnist --gan_type WGAN --epoch 3200 --batch_size 4096
+python main.py --dataset cifar10 --gan_type WGAN --epoch 800 --batch_size 1024
+python main.py --dataset cifar10 --gan_type WGAN --epoch 3200 --batch_size 4096
+python main.py --dataset mnist --gan_type WGAN_GP --epoch 50 --batch_size 64
+python main.py --dataset mnist --gan_type WGAN_GP --epoch 800 --batch_size 1024
+python main.py --dataset mnist --gan_type WGAN_GP --epoch 3200 --batch_size 4096
+python main.py --dataset cifar10 --gan_type WGAN_GP --epoch 50 --batch_size 64
+python main.py --dataset cifar10 --gan_type WGAN_GP --epoch 800 --batch_size 1024
+python main.py --dataset cifar10 --gan_type WGAN_GP --epoch 3200 --batch_size 4096
